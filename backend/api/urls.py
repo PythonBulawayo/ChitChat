e8a2e69840cc2289290df67b3bd6a20ff3040b4a
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-08-30 16:16:16 +0200

    Add profile templates and styles

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import dashboard, profile_list, profile
+
+app_name = "api"
+urlpatterns = [
+    path("", dashboard, name="dashboard"),
+    path("profile_list/", profile_list, name="profile_list"),
+    path("profile/<int:pk>/", profile, name="profile"),
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from accounts.models import Profile
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, "base.html")
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "api/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    print(profile)
+    return render(request, "api/profile.html", {"profile": profile})
