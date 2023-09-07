d5e463f07ec21a78d3b79086873483b2d6a825b2
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-09-07 22:47:32 +0200

    feat: Add initial User List API endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from accounts.models import Profile
+from accounts.models import Profile, CustomUser
+from django.conf import settings
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, ProfileSerializer
 
+User = CustomUser
 
 def dashboard(request):
     return render(request, "base.html")
@@ -15,3 +19,23 @@ def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     print(profile)
     return render(request, "api/profile.html", {"profile": profile})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = CustomUser.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user profiles to be viewed or edited
+    """
+
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
