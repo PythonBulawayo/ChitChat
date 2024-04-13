719283f90d66c74ee41f135c09a776d1532d6dbd
Mpumzee <mpumzcode@gmail.com>
2024-04-13 09:21:09 +0200

    linting issues fixed

--- frontend/src/pages/UsersList.jsx
+++ frontend/src/pages/UsersList.jsx
@@ -1,22 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function UsersList() {
-	const [users, setUsers] = useState([]);
+	// const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		fetch("http://127.0.0.1:9000/api/profiles/")
-			.then((response) => response.json())
-			.then((data) => setUsers(data));
-	}, []);
+	// useEffect(() => {
+	// 	fetch("http://127.0.0.1:9000/api/profiles/")
+	// 		.then((response) => response.json())
+	// 		.then((data) => setUsers(data));
+	// }, []);
 
 	return (
-		<div>
-			<h1>Users</h1>
-			<ul>
-				{users.map((user) => (
-					<li key={user.id}>{user.user}</li>
-				))}
-			</ul>
+		<div className="bg-red-500 p-2">
+			<div className="container mx-auto">
+				<div className="flex justify-between items-center">
+					<div className="text-white font-bold text-xl">W</div>
+					<nav>
+						<ul className="flex space-x-4">
+							<li>
+								<i className="fa-solid fa-house text-white"></i>
+								<a href="#" className="text-white hover:text-gray-300">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#" className="text-white hover:text-gray-300">
+									Companies
+								</a>
+							</li>
+							<li>
+								<a href="#" className="text-white hover:text-gray-300">
+									Profiles
+								</a>
+							</li>
+							<li>
+								<a href="#" className="text-white hover:text-gray-300">
+									Jobs
+								</a>
+							</li>
+							<li>
+								<a href="#" className="text-white hover:text-gray-300">
+									Messages
+								</a>
+							</li>
+							<li>
+								<button
+									type="button"
+									className="bg-green-500 text-white px-2 py-1 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-blue-300"
+								>
+									Notification
+								</button>
+							</li>
+						</ul>
+					</nav>
+
+					<div className="">John Doe</div>
+				</div>
+			</div>
 		</div>
 	);
 }
