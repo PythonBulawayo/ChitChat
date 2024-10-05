557e61e920129ba0c5c5be4655e815c889dc1448
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-10-05 22:07:18 +0200

    done the styling of the navbar

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,5 @@
+import userProfile from "../assets/profile.jpg";
+
+export const images = {
+	userProfile,
+};
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return <div>home</div>;
+};
